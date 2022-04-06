47dbc0f7f4c77a980aa06875eb8a68e7ef910617
Julien Ouellet <julien.ouellet@live.ca>
2022-04-06 18:58:35 -0400

    Add main script

---
+++ main.m
@@ -0,0 +1,22 @@
+%% Model initial conditions
+
+% Galvanometres
+amplitudeDACX = 2;          % V
+frequenceDACX = 100;        % Hz
+
+amplitudeDACY = 2;          % V
+frequenceDACY = 100;        % Hz
+
+% Laser
+puissanceLaser = 0.001;     % W
+distanceFocaleLaser = 47;   % cm
+
+% Barcode
+distanceLaserCode = 20;     % cm
+grossissementCode = 100;    % pourcentage
+angleAutourZ = 0;           % deg
+angleAutourX = 0;           % deg
+
+
+%% Open Simulink model
+
